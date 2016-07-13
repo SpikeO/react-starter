18db46a6158965d24fa71e42e976c4ec6ac0c25c
Michaël <michael@mopdebeeck.be>
2016-07-13 23:42:16 +0200

    CSS modules

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import styles from './Counter.css';
 /**
  * A counter button: tap the button to increase the count.
  */
@@ -13,7 +13,7 @@ class Counter extends Component {
 
   render() {
     return (
-      <button onClick={() => {
+      <button className={styles.testbtn} onClick={() => {
         this.setState({count: this.state.count + 1});
       }}>
         Count: {this.state.count}
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 var path = require('path');
 
 var config = {
-    context: path.join(__dirname, 'src'),
-    entry: [
-        './main.js',
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel'],
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader?modules"
+      }
     ],
-    output: {
-        path: path.join(__dirname, 'www'),
-        filename: 'bundle.js',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ['babel'],
-            },
-        ],
-    },
-    resolveLoader: {
-        root: [
-            path.join(__dirname, 'node_modules'),
-        ],
-    },
-    resolve: {
-        root: [
-            path.join(__dirname, 'node_modules'),
-        ],
-    },
+  },
+  resolveLoader: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
 };
 module.exports = config;
