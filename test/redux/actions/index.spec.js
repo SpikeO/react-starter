0a43b082a578b4cd217e057d5b7f8b002f11621a
Michaël <michael@mopdebeeck.be>
2016-07-31 13:07:13 +0200

    Move and rename Mocha test

--- test/redux/actions/index.spec.js
+++ test/redux/actions/index.spec.js
@@ -1,13 +1,13 @@
-import expect from 'expect'
-import * as actions from '../src/redux/actions/index'
+import expect from 'expect';
+import * as actions from '../../../src/redux/actions/index';
 
 describe('actions', () => {
   it('action should set a new book', () => {
-    const payload = {title: 'test', pages: 3};
+    const payload = { title: 'test', pages: 3 };
     const expectedAction = {
       type: actions.type,
       payload
     };
     expect(actions.selectBook(payload)).toEqual(expectedAction);
-  })
+  });
 });
